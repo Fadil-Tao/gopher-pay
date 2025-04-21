d2ab7ed6c0d69373a14c3de276afb1dcba1eb8ef
Fadil-Tao <hadadfadilah2354@gmail.com>
2025-04-21 15:20:46 +0800

    feat : user field validator

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,9 +4,9 @@ import "time"
 
 type User struct {
 	Id int `json:"id"`
-	Email string `json:"email"`
-	Name string `json:"name"`
-	Password string `json:"password,omitempty"`
+	Email string `json:"email" validate:"required"`
+	Name string `json:"name" validate:"required,min=5,max=20"`
+	Password string `json:"password,omitempty" validate:"required,min=8"`
 	Salt string
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
