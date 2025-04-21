48e327571afd42ebdbad7de34c751cd7c933ce19
Fadil-Tao <hadadfadilah2354@gmail.com>
2025-04-21 15:18:11 +0800

    chore: add new custom error

--- utils/errors/errors.go
+++ utils/errors/errors.go
@@ -5,4 +5,5 @@ var (
 	ErrNotFound = errors.New("error not found")
 	ErrInternal = errors.New("internal server error")
 	ErrIsAlreadyExist = errors.New("source already exist")
+	ErrUnauthorized = errors.New("unauthorized")
 )
