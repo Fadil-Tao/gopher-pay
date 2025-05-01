960978f96f22bdc3fd55be038a8f0ef4c1502fb5
Fadil-Tao <hadadfadilah2354@gmail.com>
2025-05-01 11:19:45 +0800

    feat: custom regex validation

--- utils/validator/validator.go
+++ utils/validator/validator.go
@@ -1,1 +1,12 @@
-package validator
+package customvalidator
+
+import (
+	"regexp"
+	"github.com/go-playground/validator/v10"
+)
+
+var amountRegex = regexp.MustCompile(`^[0-9]+(\.[0-9]{1,2})?$`) // e.g., 100, 100.50
+
+func AmountValidator(fl validator.FieldLevel) bool {
+	return amountRegex.MatchString(fl.Field().String())
+}
